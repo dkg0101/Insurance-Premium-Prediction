d70b1d1825aba279d549fd032b3da9b345772f50
Dhananjay <dkgurav0101@gmail.com>
2023-11-20 10:29:33 +0530

    modify

--- src/insurance/constant/__init__.py
+++ src/insurance/constant/__init__.py
@@ -1,2 +1,3 @@
 from src.insurance.constant.database import DATABASE_NAME,COLLECTION_NAME
 from src.insurance.constant.env_variable import MONGODB_URL_KEY
+from src.insurance.constant import training_pipeline
